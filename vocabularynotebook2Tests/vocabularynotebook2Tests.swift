f7aa43d92adbd9e4aa304345eaecb1bfe15de4bf
MIYAMOTOMASANARI <generalconan1024@gmail.com>
2016-02-20 16:13:30 +0900

    first commit

---
+++ vocabularynotebook2Tests/vocabularynotebook2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  vocabularynotebook2Tests.swift
+//  vocabularynotebook2Tests
+//
+//  Created by Masanari Miyamoto on 2016/02/20.
+//  Copyright © 2016年 Masanari Miyamoto. All rights reserved.
+//
+
+import XCTest
+@testable import vocabularynotebook2
+
+class vocabularynotebook2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
